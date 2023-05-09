b752c5d089687771b539cd5b3bfc137053038188
Crsarmv7l <85343771+Crsarmv7l@users.noreply.github.com>
2023-05-09 13:20:21 -0400

    Add RX with signal save in .ys1 format

---
+++ DaikonRX.py
@@ -0,0 +1,89 @@
+from rflib import *
+from time import sleep
+from sys import exit
+import os
+
+def write(freq, mod, baud, capture, dev=0):
+	home = os.path.expanduser('~/Saved_TX/')
+
+	yn = input("Write to file? y/N\n")
+	if yn.lower() == "y":
+		path = input("Enter the name of the save file\n")
+		f = open(home + path + ".ys1", "w")
+		freq = (freq/1000000)
+		f.write('Freq: %s\n' % freq)
+		f.write('Mod: %s\n' % mod)
+		f.write('Baud: %s\n' % baud)
+		if dev != 0:
+			f.write('Deviation: %s\n' % dev)
+		f.write('Repeat: 0\n')
+		f.write('Preamble: \n')
+		f.write('Data: %s\n' % capture)
+		f.close()
+	else:
+		print("Resuming")
+		print("Press Enter to stop\n")
+
+def main():
+	
+	freq = input("Enter freq to two decimals: (eg. 433.92) \n")
+	freq = str(freq).replace('.', '')
+	freq = int(freq)*10000
+	
+	mod = input("Number of modulation: 1. ASK/OOK / 2. FSK \n")
+	mod = int(mod)
+	
+	baud = input("Enter baud rate:")
+	baud = int(baud)
+	
+	os.system('clear')
+
+#setup rfcat
+	d = RfCat()
+	d.setModeIDLE()
+	d.setModeRX()
+	d.setFreq(freq)
+	d.setAmpMode(0)
+	print('Set Freq: %s' % freq)
+	if mod == 1:
+		d.setMdmModulation(MOD_ASK_OOK)
+		print("Set Mod: ASK")
+		mod = "ASK"
+	elif mod == 2:
+		dev = input("Enter deviation: \n")
+		dev = int(dev)
+		d.setMdmModulation(MOD_2FSK)
+		print("Set Mod: FSK")
+		mod = "FSK"
+		d.setMdmDeviatn(dev)
+		print('Set Deviation: %s' % dev)
+	else:
+		Print("Unsupported modulation")
+		d.setModeIDLE()
+		sys.exit(1)
+	d.setMdmDRate(baud)
+	print('Set Baud: %s' % baud)
+	d.setMdmChanSpc(24000)
+	d.calculatePktChanBW()
+	d.setMdmSyncMode(0)
+	d.setChannel(0)
+	d.setMaxPower()
+	d.lowball(1)
+	
+	print("Press ENTER to stop")
+	while not keystop():
+		try:
+			pkt, y = d.RFrecv()
+			capture = pkt.hex()
+			print('Received: %s\n' % capture)
+			if mod == "ASK":
+				write(freq, mod, baud, capture)
+			elif mod == "FSK":
+				write(freq, mod, baud, capture, dev)
+		except ChipconUsbTimeoutException:
+			pass 	
+	d.setModeIDLE()
+	time.sleep(1)
+
+if __name__ == '__main__':
+    main()
